e8697dd79d20089bb027305e02805a3f67dfe774
Ed Tansey <office.fwd@yahoo.com>
2021-12-01 00:43:52 +0000

    Provide and consume actions

--- src/components/App.js
+++ src/components/App.js
@@ -64,16 +64,16 @@ class App extends Component {
 
   render() {
     return (
-      <Provider value={this.state.players}>
+      <Provider value={{
+        players: this.state.players,
+        actions: {
+          changeScore: this.handleScoreChange
+        }
+      }}>
         <div className="scoreboard">
           <Header />
 
-          <PlayerList 
-            players={this.state.players} 
-            changeScore={this.handleScoreChange}
-            removePlayer={this.handleRemovePlayer}   
-          />
-          
+          <PlayerList removePlayer={this.handleRemovePlayer} />
           <AddPlayerForm addPlayer={this.handleAddPlayer} />
         </div>
       </Provider>
